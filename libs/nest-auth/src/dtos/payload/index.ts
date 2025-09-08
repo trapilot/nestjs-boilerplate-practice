049fc9b4d216b2b24a6407ffbb621fe5adc392a1
Nam Dinh <namdwb@gmail.com>
2025-09-08 16:33:36 +0700

    fix: correct auth dtos

--- libs/nest-auth/src/dtos/payload/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './auth.access-payload.dto'
-export * from './auth.refresh-payload.dto'
-export * from './auth.social.apple-payload.dto'
-export * from './auth.social.google-payload.dto'
